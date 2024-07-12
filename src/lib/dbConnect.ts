8bac0e64ade969441f82e7b397e31904b6487836
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-07-12 18:03:03 +0530

    define schemas and DB connect

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected to database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    } catch (error) {}
+}
